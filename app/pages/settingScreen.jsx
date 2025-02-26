b913181f593751f93009a8cce755946411febd1d
Ferdi <57798860+ImHyper01@users.noreply.github.com>
2025-02-26 19:46:45 +0100

    setting page styling soort van klaar, alleen nog nog meer functioneel maken

--- app/pages/settingScreen.jsx
+++ app/pages/settingScreen.jsx
@@ -1,10 +1,59 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, {useState, useEffect, useContext} from "react";
+import { Text, View, StyleSheet, Switch} from "react-native";
+import themeContext from "../theme/themeContext";
+import { SafeAreaView } from "react-native";
+
 
 export default function settingScreen() {
+    const theme = useContext(themeContext);
+    const [darkmode, setDarkMode] = useState(false);
+    
+    //opslaan in async storage + het laden uit de async storage
+  useEffect(() => {
+    const loadDarkMode = async() => {
+      const savedDarkMode = await AsyncStorage.getItem('darkMode');
+      if (savedDarkMode !== null) {
+        setDarkMode(JSON.parse(savedDarkMode));
+        EventRegister.emit('ChangeTheme', JSON.parse(savedDarkMode));
+      }
+    };
+
+    loadDarkMode();
+  }, []);
+
+  const toggleDarkMode = async (value) => {
+    setDarkMode(value);
+    EventRegister.emit('ChangeTheme', value);
+    await AsyncStorage.setItem('darkMode', JSON.stringify(value));
+  };
+
     return(
-        <View>
-            <Text>Setting Screen</Text>
-        </View>
-    )
-}
+        <SafeAreaView style={styles.container}>
+            <View style={styles.settingRow}>
+                <Text style={styles.settingLabel}>Donkere modus</Text>
+                <Switch value={darkmode} onValueChange={toggleDarkMode} />
+            </View>
+        </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 80,
+      },
+      settingRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 16,
+        alignContent: 'center',
+        gap: 20,
+      },
+      settingLabel: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+});
